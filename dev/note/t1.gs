39873e9d5f91d56d4eb59ced21a09674b9054216
we684123 <we684123@gmail.com>
2020-08-16 19:12:52 +0800

    解構賦值模式修改

---
+++ dev/note/t1.gs
@@ -0,0 +1,70 @@
+{
+  "update_id": 33321849,
+  "callback_query": {
+    "id": "889120953351445375",
+    "from": {
+      "id": 207014603,
+      "is_bot": false,
+      "first_name": "永格天",
+      "last_name": "(則天)",
+      "username": "we684123",
+      "language_code": "zh-hant"
+    },
+    "message": {
+      "message_id": 27,
+      "from": {
+        "id": 909196871,
+        "is_bot": true,
+        "first_name": "此方",
+        "username": "CiFang_bot"
+      },
+      "chat": {
+        "id": 207014603,
+        "first_name": "永格天",
+        "last_name": "(則天)",
+        "username": "we684123",
+        "type": "private"
+      },
+      "date": 1597412048,
+      "reply_to_message": {
+        "message_id": 10,
+        "from": {
+          "id": 909196871,
+          "is_bot": true,
+          "first_name": "此方",
+          "username": "CiFang_bot"
+        },
+        "chat": {
+          "id": 207014603,
+          "first_name": "永格天",
+          "last_name": "(則天)",
+          "username": "we684123",
+          "type": "private"
+        },
+        "date": 1570349202,
+        "text": "12"
+      },
+      "text": "456",
+      "reply_markup": {
+        "inline_keyboard": [
+          [{
+            "text": "⭕️",
+            "callback_data": "event_join_⭕️"
+          }, {
+            "text": "❓",
+            "callback_data": "event_join_❓"
+          }, {
+            "text": "❌",
+            "callback_data": "event_join_❌"
+          }],
+          [{
+            "text": "Leave",
+            "callback_data": "event_leave"
+          }]
+        ]
+      }
+    },
+    "chat_instance": "-630598374506774449",
+    "data": "event_join_⭕️"
+  }
+}
---
+++ test/test_function.js
@@ -0,0 +1,74 @@
+"use strict";
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
+function unit_test() {
+    // @ts-ignore
+    var logger = new BetterLogger.BetterLogger();
+    var levels = logger.levels;
+    logger.set_level(levels.DEBUG);
+    logger.set_sheet_id("1ADCWcQ3J7uXtRA8r5zRIz1qweAtfKD0E53eLGra66T8");
+    logger.set_sheet_page_name("test");
+    logger.set_use_sheet(true);
+    var bot = new tgbot("909196871:AAHFdyF9E_LhvjjPePs6JhNHVtDU79unK4I");
+    var i = 0;
+    i++;
+    logger.info("i = " + i);
+    try {
+        logger.info("bot.deleteWebhook()");
+        logger.info(bot.deleteWebhook());
+    }
+    catch (error) {
+        logger.error(error);
+    }
+    i++;
+    logger.info("i = " + i);
+    try {
+        logger.info("bot.getUpdates()");
+        logger.info(bot.getUpdates());
+    }
+    catch (error) {
+        logger.error(error);
+    }
+    i++;
+    logger.info("i = " + i);
+    try {
+        logger.info("bot.setWebhook()");
+        logger.info(bot.setWebhook({
+            "url": "https://script.google.com/macros/s/AKfycbw3DyTC9Oh3j3uNukl4V8SUIn4sN3zAz1PkLw6OKwRdfbs-BW0F/exec",
+            "max_connections": 1
+        }));
+    }
+    catch (error) {
+        logger.error(error);
+    }
+    i++;
+    logger.info("i = " + i);
+    try {
+        logger.info("bot.getWebhookInfo()");
+        logger.info(bot.getWebhookInfo());
+    }
+    catch (error) {
+        logger.error(error);
+    }
+    i++;
+    logger.info("i = " + i);
+    try {
+        logger.info("bot.getMe()");
+        logger.info(bot.getMe());
+    }
+    catch (error) {
+        logger.error(error);
+    }
+    i++;
+    logger.info("i = " + i);
+    try {
+        logger.info("bot.sendMessage()");
+        logger.info(bot.sendMessage({
+            "chat_id": '207014603',
+            "text": "*bold *text*_italic *text___underline__~strikethrough~*bold _italic bold ~italic bold strikethrough~ __underline italic bold___ bold*[inline URL](http://www.example.com/)[inline mention of a user](tg://user?id=123456789)`inline fixed-width code``"(__makeTemplateObject(["pre-formatted fixed-width code block"], ["\\\npre-formatted fixed-width code block\\\n"]))(__makeTemplateObject([""], [""]))
+        }(__makeTemplateObject([""], [""]))(__makeTemplateObject(["pythonpre-formatted fixed-width code block written in the Python programming language"], ["python\\\npre-formatted fixed-width code block written in the Python programming language\\\n"]))(__makeTemplateObject([""], [""]))(__makeTemplateObject(["\n    })\n    )\n  } catch (error) { logger.error(error) }\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n}\n"], ["\n    })\n    )\n  } catch (error) { logger.error(error) }\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n}\n"]))));
+    }
+    finally { }
+}
